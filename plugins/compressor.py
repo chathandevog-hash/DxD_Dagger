fbb61a029229ec0dcd45011d86d570b3d493f819
chathandevog-hash <chathandevog@gmail.com>
2026-01-13 16:53:54 +0530

    Update compressor.py

--- plugins/compressor.py
+++ plugins/compressor.py
@@ -31,7 +31,7 @@ async def q_choose(client, query):
     await query.message.reply_text("✏️ Send rename name:")
     await query.answer()
 
-@Client.on_message(filters.text & ~filters.command)
+@Client.on_message(filters.text & ~filters.command())
 async def comp_run(client, message):
     uid = message.from_user.id
     if uid not in STATE:
