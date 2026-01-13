7362f659645d2d4890bcea6cb209112bc6c79931
chathandevog-hash <chathandevog@gmail.com>
2026-01-13 16:29:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
-API_ID = 12345
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = "36360287"
+API_HASH = "4283a743d393b1094600a7065a50b3c0"
+BOT_TOKEN = "8300933730:AAGnORVJqH1g2Cbuuf2PDXhWiB99H9eWGyc"
 
 # Force subscribe
 FORCE_SUB = True
-FORCE_SUB_CHANNEL = "@YourChannel"
-FORCE_SUB_CHANNEL_ID = -1001234567890
+FORCE_SUB_CHANNEL = "https://t.me/jb_links"
+FORCE_SUB_CHANNEL_ID = "-1002295034629"
 
 # Admins
-ADMINS = [123456789]
+ADMINS = [8417661273]
 
 # Log channel (optional)
 LOG_CHANNEL = -1001234567890
 
 # Database
-MONGO_URL = "mongodb+srv://user:pass@cluster.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URL = "mongodb+srv://chathanbot:chathan@cluster0.edekjxb.mongodb.net/?appName=Cluster0"
 DB_NAME = "RenameProBotDB"
 
 # Batch limit
