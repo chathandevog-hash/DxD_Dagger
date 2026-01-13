06c20150f53b8fb4ee975bd854d6b0653dd4e513
chathandevog-hash <chathandevog@gmail.com>
2026-01-13 16:09:18 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+from utils.buttons import main_menu
+from plugins.force_sub import force_sub_check
+from database.users import add_user
+
+@Client.on_message(filters.command("start"))
+async def start_cmd(client, message):
+    ok = await force_sub_check(client, message)
+    if not ok:
+        return
+
+    await add_user(message.from_user.id)
+
+    await message.reply_text(
+        "👋 Hi!\nSend any file/video.\n\nChoose option 👇",
+        reply_markup=main_menu()
+    )
