a14182605fa82ee846151bb87f24ab6f70ac409c
chathandevog-hash <chathandevog@gmail.com>
2026-01-13 15:55:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+API_ID = 12345
+API_HASH = "YOUR_API_HASH"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+# Force subscribe
+FORCE_SUB = True
+FORCE_SUB_CHANNEL = "@YourChannel"
+FORCE_SUB_CHANNEL_ID = -1001234567890
+
+# Admins
+ADMINS = [123456789]
+
+# Log channel (optional)
+LOG_CHANNEL = -1001234567890
+
+# Database
+MONGO_URL = "mongodb+srv://user:pass@cluster.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "RenameProBotDB"
+
+# Batch limit
+BATCH_LIMIT = 50
